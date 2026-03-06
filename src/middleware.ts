911773c5e328e54e6cf2b93a55899550d24e59e9
Rustless <rudy_curioso@outlook.com>
2026-03-06 04:03:02 -0600

    configuracion de cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,5 +11,10 @@ export default clerkMiddleware(async (auth, req) => {
 })
 
 export const config = {
-  matcher: ['/api/(.*)'],
-}
+  matcher: [
+    // Omitir archivos internos de Next.js y estáticos (imagenes, etc)
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Siempre ejecutar para rutas de API y trpc
+    '/(api|trpc)(.*)',
+  ],
+};
